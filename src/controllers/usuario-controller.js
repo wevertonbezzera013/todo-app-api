38d9aa3cdfa7d4a67181bfd5ee59420dd8aaca74
Weverton <wevertonbezzera013@gmail.com>
2021-02-23 11:41:39 -0300

    Update usuario-controller.js

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -1,56 +1,52 @@
-const express = require('express');
-const Usuario = require('../models/usuario');
-module.exports = (app, dados) => {
-  app.get('/usuario', (req, res) =>
-  {
-    res.send(dados.usuario);
-  })
-  app.get('/usuario/:email', (req, res) => {
-    let resposta = [];
-    for (let i=0; i < dados.usuario.length; i++) {
-      
-      if (dados.usuario[i].email == req.params.email) {
-        resposta.push(dados.usuario[i])
-      }
-    }
-    res.send(resposta);
-  })
-  app.post('/usuario', (req, res) => {
-    const novoUsuario = req.body;
-    dados.usuario.push(novoUsuario);
-    res.send('<h1>Rota para a Tarefa</h1>');
-  })
+//const Usuario = require('../models/usuarios')
+const USUARIOS = require('../DAO/usuarios-dao')
 
-  app.delete('/usuario/:email', (req, res) =>{
+module.exports = (app, bd) => {
 
-    let listaUsuario = [];
-
-    for (let i=0; i < dados.usuario.length; i++) {
-      if (req.params.email !== dados.usuario[i].email) {
-        listaUsuario.push(dados.usuario[i]);
-      }
-    }
-    if(dados.usuario.length != listaUsuario.length) {
-      
-      res.send(`Usuario ${req.params.email} foi removido!`)
-    } else {
-      res.send(`Usuario ${req.params.email}  não foi encontrado!`);
-    }
-    dados.usuario = listaUsuario;
-    
-  })
-
-const atualizaRegistro = (email, body) => {
-  for (let usr of dados.usuario) {
-    if (usr.email === email) {
-      usr.nome = body.nome;
-      usr.senha = body.senha;
-    }
-  }
-}
-
-app.put('/usuario/:email', (req, res)=> {
-  atualizaRegistro(req.params.email, req.body);
-  res.send('Dados do usuario atualizados')
-  }) 
+    const USUARIOS = new USUARIOS(bd);
+    app.get('/user', async (req, res) => {
+        try {
+            const usuarios = await USUARIOS.lista()
+            res.send(usuarios);
+        }
+        catch (erro) {
+            res.send(erro)
+        }
+    });
+    app.get('/user/:id', async(req, res) => {
+        try{
+            const USERparametro = await USUARIOS.parametros(req.params.id)
+            res.send(USERparametro)
+        }
+        catch(erro){
+            res.send(erro)
+        }
+    });
+    app.post('/user', async (req, res) => {
+        try {
+            const ADDuser = await USUARIOS.adicionar([req.body.nome, req.body.email, req.body.senha])
+            res.send(ADDuser)
+        }
+        catch (erro) {
+            res.send(erro)
+        }
+    });
+    app.delete('/user/:id', async (req, res) => {
+        try {
+            const DELuser = await USUARIOS.deletar(req.params.id)
+            res.send(DELuser)
+        }
+        catch(erro){
+            res.send(erro)
+        }
+    });
+    app.put('/user/:id', async(req, res) => {
+        try {
+            const ATLuser = await USUARIOS.atualizar([req.body.nome, req.body.email, req.body.senha, req.params.id])
+            res.send(ATLuser)
+        }
+        catch(erro){
+            res.send(erro)
+        }
+    });
 }
